8857f325a64700d26de583728bd2ba07ea5af937
User <User@DESKTOP-8KA60R8>
2022-09-30 22:57:37 +0300

    Добавьте файлы проекта.

---
+++ CFU-Lesson-4/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFU_Lesson_4
+{
+    internal class Program
+    {
+        enum card
+        {
+            шестерк, семерка, восьмерка, девятка, десятка, валет, дама, король, туз
+        }
+        public enum dninedel
+        {
+            Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6, Sunday = 7
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Задание 1. Вася и окно");
+            Console.WriteLine("Введите длину форточки");
+            double a1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите ширину форточки");
+            double b1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите диаметр головы Васи");
+            double d1 = double.Parse(Console.ReadLine());
+            if (a1 - d1 >= 1 && b1 - d1 >= 1)
+            {
+                Console.WriteLine("Голова пролезет");
+            }
+            else
+            {
+                Console.WriteLine("Упс голова не пролезет");
+
+
+
+            Console.WriteLine("Задание 2. Таблица умножения на число n");
+            Console.WriteLine("Введите число n");
+            int n2 = int.Parse(Console.ReadLine());
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine($"n*{i} = {n2 * i}");
+            }
+
+            /*
+            Console.WriteLine("Задание 3. Принимает на вход число и возвращает слово");
+            Console.WriteLine("Введите число");
+            int a3 = int.Parse(Console.ReadLine());
+            if (a3 % 15 == 0)
+            {
+                Console.WriteLine("БэнгБум");
+            }
+            else if (a3 % 5 == 0)
+            {
+                Console.WriteLine("Бум");
+            } 
+            else if (a3 % 3 == 0)
+            {
+                Console.WriteLine("Бэнг");
+            }
+            else
+            {
+                Console.WriteLine("Мимо");
+            }
+            */
+
+
+            Console.WriteLine("Задание 3. Принимает на вход число и возвращает слово");
+            Console.WriteLine("Введите число");
+            int a3 = int.Parse(Console.ReadLine());
+            string Answer;
+            Answer = a3 % 15 == 0 ? "BengBoom" : a3 % 5 == 0 ? "Boom" : a3 % 3 == 0 ? "Beng" : "None";
+            Console.WriteLine(Answer);
+
+
+
+            Console.WriteLine("Задание 4. Строка с заглавными буквами");
+            Console.WriteLine("Введите строку");
+            string lena = Console.ReadLine();
+            string a = " "; // Чар пустой
+            int dlena = lena.Length;
+            while (dlena > 0)
+            {
+                char c4 = lena[dlena - 1];
+                if (char.IsUpper(c4) ^ c4 == a[0])
+                {
+                    dlena--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(dlena == 0 ? true : false);
+
+
+            Console.WriteLine("Задание 5.Вывести овец");
+            Console.WriteLine("Введите сколько овец в стаде");
+            int n5 = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n5; i++)
+            {
+                Console.Write($"{i + 1} овца...");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Задание 6.Не понял как делать");
+            Console.WriteLine("Задание 7.Не понял как делать");
+          
+
+
+            Console.WriteLine("Задание 8. Проход по массиву ");
+            int c8 = 0;
+            string[] stroki = { "Hello Kitty", "Mama", "JOpa", "Barbie doll" };
+            foreach (string i in stroki)
+            {
+                if (i == "Hello Kitty" ^ i == "Barbie doll")
+                {
+                    c8++;
+                }
+            }
+            Console.WriteLine($"В сумке было {c8} Кукол(ы)");
+
+
+
+            Console.WriteLine("Задание 9. Дни недели");
+                Console.WriteLine("Введите цифру дня недели");
+            int n9 = int.Parse(Console.ReadLine());
+            if (n9 > 0 && n9 < 7)
+            {
+                Console.WriteLine($"Ваш день недели -{(dninedel)n9}");
+            }
+            else
+            {
+                Console.WriteLine("Под таким номером нет дня недели");
+            }
+
+
+            
+            Console.WriteLine("Задание 10. Массив по возрастанию");
+            int c;
+            int[] arr1 = { 32, 13123, 222, -9, 0 };
+            int[] arr2 = { 32, 13123, 222, -9, 0 };
+            int[] arr3 = { 32, 13123, 222, -9, 0 };
+            int lena1 = arr2.Length;
+            Array.Sort(arr1);
+            for (int ik = 0; ik < lena1; ++ik)
+            {
+                Console.Write(arr1[ik] + " ");
+            }
+            Console.WriteLine();
+
+
+            for (int b = 0; b < lena1; b++)
+            {
+                for (int i = 0; i < arr2.Length - 1; i++)
+                {
+                    if (arr2[i] > arr2[i + 1])
+                    {
+                        c = arr2[i];
+                        arr2[i] = arr2[i + 1];
+                        arr2[i + 1] = c;
+                    }
+                }
+            }
+            for (int i = 0; i < lena1; ++i)
+            {
+                Console.Write(arr2[i] + " ");
+            }
+            Console.WriteLine();
+
+
+            for (int i = 0; i < arr3.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr3.Length; j++)
+                {
+                    if (arr3[i] > arr3[j])
+                        (arr3[i], arr3[j]) = (arr3[j], arr3[i]);
+                }
+            }
+
+
+
+                Console.WriteLine("Задание 11. По возрастанию");
+            int[] arr = { 1, 23, 421, 124 };
+            int call = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    Console.WriteLine($"Строка сломалась на {i} элементе массива");
+                    break;
+                    call++;
+                }
+            }
+            Console.WriteLine(call == arr.Length ? "Массив упорядочен по возрастанию" : "");
+
+
+
+
+
+
+
+
+            }
+        }
+
+    }
+}
---
+++ T00Much/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T00Much
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 4.1 Преобразовать день в дату");
+            Console.WriteLine("Введите число от 1 до 365");
+            int a = int.Parse(Console.ReadLine());
+            DateTime data;
+            DateTime dateTime = new DateTime(2022,1,1);
+            dateTime.AddYears(2022);
+            data =dateTime.AddDays(a-1);
+            Console.WriteLine(data.ToString("Дата: d MMMM"));
+
+
+            Console.WriteLine("Упражнение 4.2 Преобразовать день в дату");
+            Console.WriteLine("Введите число от 1 до 365");
+            int a2 = int.Parse(Console.ReadLine());
+            if (a2 <1 ^ a2 > 365)
+            {
+                Console.WriteLine("Неправильно попробуй еще раз");
+            }
+            else
+            {
+                DateTime data2;
+                DateTime dateTime2 = new DateTime(2022, 1, 1);
+                dateTime2.AddYears(2022);
+                data2 = dateTime.AddDays(a - 1);
+                Console.WriteLine(data2.ToString("Дата: d MMMM"));
+            }
+            
+
+    
+            Console.WriteLine("Домашнее Задание 4.1 Преобразовать день в дату");
+            Console.WriteLine("Введите число от 1 до 365");
+            int a3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите год");
+            int god = int.Parse(Console.ReadLine());
+            if (a3 < 1 ^ a3 > 365)
+            {
+                Console.WriteLine("Неправильно попробуй еще раз");
+            }
+            else
+            {
+                if ((god % 4 == 0 && god % 100 != 0) ^ (god % 400 == 0)) //высокосный
+                {
+                    if (a3 == 60)
+                    {
+                        Console.WriteLine($"29 Февраля {god} года");
+                    }
+                    else
+                    {
+                        if (a3 < 60)
+                        {
+                            DateTime data3;
+                            DateTime dateTime3 = new DateTime(2022, 1, 1);
+                            dateTime3.AddYears(2022);
+                            data3 = dateTime3.AddDays(a3 - 1);
+                            Console.WriteLine(data3.ToString("Дата: d MMMM"));
+                        }
+                        else
+                        {
+                            DateTime data5;
+                            DateTime dateTime5 = new DateTime(2022, 1, 1);
+                            dateTime5.AddYears(2022);
+                            data5 = dateTime5.AddDays(a3 - 2);
+                            Console.WriteLine(data5.ToString("Дата: d MMMM"));
+                        }
+                    }
+                }
+                else
+                {
+                    DateTime data4;
+                    DateTime dateTime4 = new DateTime(2022, 1, 1);
+                    dateTime4.AddYears(2022);
+                    data4 = dateTime4.AddDays(a3 - 1);
+                    Console.WriteLine(data4.ToString("Дата: d MMMM"));
+                }
+            }
+        }
+    }
+}
